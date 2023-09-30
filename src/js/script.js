96d71a6c8769e403589f5731f7f88331051432e4
Kuba1507 <kuba.romanski@onet.com.pl>
2023-09-30 16:42:42 +0200

    repaired nav for mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
-const burgerBtn = document.querySelector(".navbar__burger-btn");
-const navItems = document.querySelector(".navbar__items");
-const allNavItem = document.querySelectorAll(".navbar__items-item");
+const navbar = document.querySelector('.navbar')
+const burgerBtn = document.querySelector('.navbar__burger-btn')
+const navItems = document.querySelector('.navbar__items')
+const allNavItem = document.querySelectorAll('.navbar__items-item')
 
 const handleNav = () => {
-	navItems.classList.toggle("handling-nav");
-};
+	navItems.classList.toggle('handling-nav')
+	document.body.classList.toggle('fixed-body')
+}
 
-burgerBtn.addEventListener("click", handleNav);
+burgerBtn.addEventListener('click', handleNav)
