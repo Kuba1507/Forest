8083e9cdcd2d0227b0250a98cdf68c2b6945706d
Kuba1507 <kuba.romanski@onet.com.pl>
2023-10-03 09:19:26 +0200

    another offers section set for tablets and desktops

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,20 @@
-const navbar = document.querySelector('.navbar')
-const burgerBtn = document.querySelector('.navbar__burger-btn')
-const navItems = document.querySelector('.navbar__items')
-const allNavItem = document.querySelectorAll('.navbar__items-item')
+const navbar = document.querySelector(".navbar");
+const burgerBtn = document.querySelector(".navbar__burger-btn");
+const navItems = document.querySelector(".navbar__items");
+const allNavItem = document.querySelectorAll(".navbar__items-item");
 
 const handleNav = () => {
-	navItems.classList.toggle('handling-nav')
-	document.body.classList.toggle('fixed-body')
-}
+	navItems.classList.toggle("handling-nav");
+	document.body.classList.toggle("fixed-body");
+};
 
-burgerBtn.addEventListener('click', handleNav)
+const closeNav = () => {
+	navItems.classList.remove("handling-nav");
+	document.body.classList.remove("fixed-body");
+};
+
+allNavItem.forEach((item) => {
+	item.addEventListener("click", closeNav);
+});
+
+burgerBtn.addEventListener("click", handleNav);
